ad3dbec90fad249dd0594f5d844da1d1a728e199
Shreyansh <shreyansh@example.com>
2026-08-23 21:30:26 +0530

    number hashing using map

--- practive/Hashing.cpp
+++ practive/Hashing.cpp
@@ -40,23 +40,24 @@ int main(){
     //     cout<<hash[c]<<endl;
     // }
 
-    // number hashing using map 
-    int n;
-    cin>>n;
-    int arr[n];
-    map<int,int>mpp;
-    for (int i=0;i<n;i++){
-        cin>>arr[i];
-        // precompute
-        mpp[arr[i]]++;
-    }
-    int q;
-    cin>>q;
-    while(q--){
-        int number ;
-        cin>>number;
-        cout<<mpp[number]<<endl;
-    }
 
+    // number hashing using map
+    // int n;
+    // cin>>n;
+    // int arr[n];
+    // map<int,int>mpp;
+    // for (int i=0;i<n;i++){
+    //     cin>>arr[i];
+    //     // precompute
+    //     mpp[arr[i]]++;
+    // }
+    // int q;
+    // cin>>q;
+    // while(q--){
+    //     int number ;
+    //     cin>>number;
+    //     cout<<mpp[number]<<endl;
+    // }
 
+    
 }
